4825e95724a63cada419eb9bb94eb23f21ab830f
AbdullahAlKhafajiDev <abdullahalkafajy321@gmail.com>
2023-04-22 08:55:32 -0500

    Scaled everything to a 100% browser viewing scale.

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const me_to_name = document.querySelector(".welcome-block span")
+const leftScrollBackground = document.querySelector('.side-line')
 
+leftScrollBackground.style.height = `${bodyHeightFinder() * 0.7}px`
 
 //for changing the ME to ABDULLAH AL KHAFAJI.
 window.addEventListener('scroll', (e)=>{
     fillScrollBar = document.querySelector('.fill-side-line')
     percentageScrolled = getScrollPercent() * 0.01
     console.log(percentageScrolled)
-    let i = 1;
+
     //changes name on scroll.
     if (percentageScrolled == 0){
         me_to_name.innerHTML = "me"
+        fillScrollBar.style.height = `0px`
     }
     else {
         me_to_name.innerHTML = "Abdullah Al Khafaji";
-        fillScrollBar.style.height = `${percentageScrolled * 2500}px`;
+        fillScrollBar.style.height = `${percentageScrolled * (bodyHeightFinder() * 0.7)}px`;
     }
     
 })
@@ -26,3 +29,10 @@ function getScrollPercent() {
     return (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
 }
 
+
+
+function bodyHeightFinder(){
+    let body = document.body, html = document.documentElement;
+    let bodyHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+    return bodyHeight;
+}
