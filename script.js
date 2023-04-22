c14b4a74e6e8aff72eedb8676d5a8c0452a6c00a
AbdullahAlKhafajiDev <abdullahalkafajy321@gmail.com>
2023-04-22 09:42:21 -0500

    Adjusted left margin, and name change animation.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const me_to_name = document.querySelector(".welcome-block span")
 const leftScrollBackground = document.querySelector('.side-line')
-
 leftScrollBackground.style.height = `${bodyHeightFinder() * 0.7}px`
+
+var asyncFuncWorking = false; //global variable to prevent repeated function calls.
+var inFirstSection = true;
 
 //for changing the ME to ABDULLAH AL KHAFAJI.
 window.addEventListener('scroll', (e)=>{
@@ -13,12 +15,16 @@ window.addEventListener('scroll', (e)=>{
     if (percentageScrolled == 0){
         me_to_name.innerHTML = "me"
         fillScrollBar.style.height = `0px`
+        inFirstSection = true;
     }
     else {
-        me_to_name.innerHTML = "Abdullah Al Khafaji";
+        if (asyncFuncWorking == false && inFirstSection == true){
+            slowTextTransformer(me_to_name, "Abdullah Al Khafaji")
+        }
+        inFirstSection = false;
+        // me_to_name.innerHTML = "Abdullah Al Khafaji";
         fillScrollBar.style.height = `${percentageScrolled * (bodyHeightFinder() * 0.7)}px`;
     }
-    
 })
 
 function getScrollPercent() {
@@ -29,10 +35,26 @@ function getScrollPercent() {
     return (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
 }
 
-
-
 function bodyHeightFinder(){
     let body = document.body, html = document.documentElement;
     let bodyHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
     return bodyHeight;
+}
+
+function slowTextTransformer(elementObject, newString){
+    asyncFuncWorking = true;
+    let delay = 0;
+    let speed = 30;
+    let word = '';
+    
+    for (let letter of newString){
+        setTimeout(() => {
+            word += letter;
+            elementObject.innerHTML = word;
+            if (word == newString){
+                console.log(`text conversion of "${newString}" complete.`)
+                asyncFuncWorking = false;
+            }
+        }, delay += speed);
+    }
 }
