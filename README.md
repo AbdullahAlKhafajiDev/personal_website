9f471d8865acb33f0b608f26e37b3da7cb90bb22
AbdullahAlKhafajiDev <abdullahalkafajy321@gmail.com>
2023-04-22 06:02:29 -0500

    testing a push.

---
+++ README.md
@@ -0,0 +1,2 @@
+# personal_website
+This repo will be the starting point to making a personal website that introduces myself, Abdullah Al Khafaji, to potential employers or clients. I aim to illustrate some experience in web design including front-end development and back-end development. 
---
+++ script.js
@@ -0,0 +1,28 @@
+const me_to_name = document.querySelector(".welcome-block span")
+
+
+//for changing the ME to ABDULLAH AL KHAFAJI.
+window.addEventListener('scroll', (e)=>{
+    fillScrollBar = document.querySelector('.fill-side-line')
+    percentageScrolled = getScrollPercent() * 0.01
+    console.log(percentageScrolled)
+    let i = 1;
+    //changes name on scroll.
+    if (percentageScrolled == 0){
+        me_to_name.innerHTML = "me"
+    }
+    else {
+        me_to_name.innerHTML = "Abdullah Al Khafaji";
+        fillScrollBar.style.height = `${percentageScrolled * 2500}px`;
+    }
+    
+})
+
+function getScrollPercent() {
+    var h = document.documentElement, 
+        b = document.body,
+        st = 'scrollTop',
+        sh = 'scrollHeight';
+    return (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
+}
+
